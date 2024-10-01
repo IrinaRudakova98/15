612028822cf2731b04bad12cc8414b9d6520b93f
IrinaRudakova98 <komarova-ira@mail.ru>
2024-10-01 14:56:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import TodoList from "./Todo"
+
+
+function App() {
+  
+
+  return (
+    <main>
+      <div className="container">
+        <h1>todo list</h1>
+        <TodoList />
+      </div>
+    </main>
+  )
+}
+
+export default App
